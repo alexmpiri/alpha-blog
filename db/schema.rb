52f0ebaa81615b5fc90747649b2143cfdad6d760
alexmpiri <alexmpiri@gmail.com>
2016-12-06 19:19:17 +0200

    Signup users from UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206162227) do
+ActiveRecord::Schema.define(version: 20161206165705) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
